c79ed3bf46c7dd8481ff154c684b92d2648ecf4d
Dennis-van-Gils <vangils.dennis@gmail.com>
2018-10-11 19:01:06 +0200

    Bump

--- Matlab/debug_validate_decimate.m
+++ Matlab/debug_validate_decimate.m
@@ -1,7 +1,7 @@
 mht = MHT_readFile('181011_132958.txt', true);
 
-%this_series = mht.T_ambient;
 this_series = mht.Q_tunnel;
+%this_series = mht.Q_tunnel;
 
 f_s = 1/mean(diff(mht.time)); % Original sampling frequency [Hz]
 f3dB_LP = 0.1;                % Low-pass cut-off frequency: 0.1 [Hz]
@@ -12,5 +12,5 @@ this_series_LP = filtfilt(filt_b, filt_a, this_series);
 h1 = figure(2); clf
 plot(mht.time, this_series, 'x-')
 hold on
-plot(mht.time, this_series_LP, '-r')
+plot(mht.time, this_series_LP, '-r', 'lineWidth', 2)
 xlim([200 360])
